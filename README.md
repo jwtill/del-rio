9ecfb66e0da6dc8c9e5fbe680ee802312c861cb9
Josh Tillinghast <jwtill@icloud.com>
2022-04-22 08:47:35 -0700

    Add component tree to README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+![component tree](./src/img/del-rio.png "Component Tree")
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import KegControl from "./KegControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <KegControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
